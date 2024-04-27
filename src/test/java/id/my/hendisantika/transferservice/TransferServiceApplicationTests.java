01258a8f3862542aeef7b643eeb47aaf34be9a1c
hendisantika <hendisantika@yahoo.co.id>
2024-04-28 06:52:03 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/transferservice/TransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.transferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

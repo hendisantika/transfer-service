a046812c4175260d8b12f360cdeda39fbf156594
hendisantika <hendisantika@yahoo.co.id>
2024-04-28 07:09:11 +0700

    Add add API in DepositController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/transferservice/controller/DepositController.java
+++ src/main/java/id/my/hendisantika/transferservice/controller/DepositController.java
@@ -1,9 +1,22 @@
 package id.my.hendisantika.transferservice.controller;
 
+import id.my.hendisantika.transferservice.dto.ApiErrorResponse;
+import id.my.hendisantika.transferservice.dto.DepositRequestDto;
+import id.my.hendisantika.transferservice.dto.DepositResponseDto;
 import id.my.hendisantika.transferservice.service.DepositService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.servlet.ServletRequest;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +39,18 @@ public class DepositController {
     static final String REQUEST_UID = "requestUid";
 
     private final DepositService depositService;
+
+    @Operation(summary = "Add deposit request")
+    @ApiResponse(responseCode = "201", content = @Content(schema = @Schema(implementation = DepositResponseDto.class)))
+    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @PostMapping
+    public ResponseEntity<DepositResponseDto> add(@Valid @RequestBody DepositRequestDto requestDto, ServletRequest httpServletRequest) {
+        httpServletRequest.setAttribute(REQUEST_UID, requestDto.requestUid());
+        var createdDeposit = depositService.add(requestDto.toDomain());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(DepositResponseDto.fromDomain(createdDeposit));
+    }
 
 }

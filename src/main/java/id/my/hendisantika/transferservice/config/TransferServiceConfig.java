a861b675ea7eb57a07460fdf1b5bd5f5533627fa
hendisantika <hendisantika@yahoo.co.id>
2024-04-28 07:01:54 +0700

    Add TransferServiceConfig 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/transferservice/config/TransferServiceConfig.java
@@ -0,0 +1,34 @@
+package id.my.hendisantika.transferservice.config;
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : transfer-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/28/24
+ * Time: 07:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@Slf4j
+public class TransferServiceConfig {
+
+    // Monitoring beans
+    @Bean
+    ObservationRegistry observationRegistry() {
+        return ObservationRegistry.create();
+    }
+
+    // To have the @Observed support we need to register this aspect
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+}

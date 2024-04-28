237e82f74c54cc087738f8ae1c73858b85fa05aa
hendisantika <hendisantika@yahoo.co.id>
2024-04-28 07:21:57 +0700

    Add shouldAddDeposit_OnNewDepositRequest Test Case 🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/transferservice/TransferServiceApplicationTests.java
+++ src/test/java/id/my/hendisantika/transferservice/TransferServiceApplicationTests.java
@@ -1,5 +1,8 @@
 package id.my.hendisantika.transferservice;
 
+import id.my.hendisantika.transferservice.domain.Deposit;
+import id.my.hendisantika.transferservice.dto.DepositResponseDto;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +13,8 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Testcontainers
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,10 +24,29 @@ class TransferServiceApplicationTests {
     static final String API_URL = "/api/deposits";
     private static final int CUSTOMER_NOT_FOUND_ERROR_CODE = 404;
     private static final int VALIDATION_ERROR_CODE = 400;
+
     @Container
     @ServiceConnection
-    static PostgreSQLContainer postgres = new PostgreSQLContainer<>("postgres:16-alpine3.19");
-    @Autowired
-    WebTestClient webTestClient;
+    private static final PostgreSQLContainer postgres = new PostgreSQLContainer<>("postgres:16-alpine3.19");
 
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void shouldAddDeposit_OnNewDepositRequest() {
+        Deposit deposit = DepositFixture.createDeposit();
+        DepositResponseDto depositResponse = webTestClient
+                .post().uri(API_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(deposit)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(DepositResponseDto.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertThat(depositResponse).isNotNull();
+        assertThat(depositResponse.requestUid()).isEqualTo("1111-3333");
+    }
 }

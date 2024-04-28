dbacd026292855f47af78cd0c70d9c5e7f79aa47
hendisantika <hendisantika@yahoo.co.id>
2024-04-28 07:23:12 +0700

    Add DepositFixture 🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/transferservice/fixture/DepositFixture.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.transferservice.fixture;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : transfer-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/28/24
+ * Time: 07:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class DepositFixture {
+    public static final String REQUEST_UID = "1111-3333";
+    public static final long CUSTOMER_ID = 1;
+    public static final String FROM_ACCOUNT_NUMBER = "22351111";
+    public static final String TO_ACCOUNT_NUMBER = "55761161";
+    public static final BigDecimal AMOUNT = new BigDecimal("150.00");
+}

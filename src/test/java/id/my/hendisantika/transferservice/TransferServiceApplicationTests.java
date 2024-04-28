1cfb0ff587c02d3d519efffdd18ecb61f0581c0a
hendisantika <hendisantika@yahoo.co.id>
2024-04-28 07:20:09 +0700

    Modify TransferServiceApplicationTests 🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/transferservice/TransferServiceApplicationTests.java
+++ src/test/java/id/my/hendisantika/transferservice/TransferServiceApplicationTests.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.transferservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class TransferServiceApplicationTests {
 
     @Test
